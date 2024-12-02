7d16a1ddfdd5b98be1c076bf89b722822ff14adb
brayanfloresf2 <brayanfloresf2@gmail.com>
2024-12-02 16:32:47 +0000

    Listo el responsive de la pagina AdminPanel

--- src/Components/TablePanel.jsx
+++ src/Components/TablePanel.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 export default function TablePanel() {
-
     const [view, setView] = useState("companies");
     const [data, setData] = useState({
         companies: [
@@ -24,103 +23,100 @@ export default function TablePanel() {
         setData(updatedData);
     };
 
-
-    return(
+    return (
         <>
-         {/* Table */}
-         <div className="border rounded-t-lg shadow-md bg-white w-9/12">
-                    <div className="flex justify-center border border-none rounded-t-lg">
-                        <button
-                            className={`px-4 py-2 rounded-tl-lg w-full ${view === "companies" ? "bg-orange-500 text-white" : "bg-gray-200 text-gray-700"}`}
-                            onClick={() => setView("companies")}
-                        >
-                            Companies
-                        </button>
-                        <button
-                            className={`px-4 py-2 rounded-tr-lg w-full ${view === "authors" ? "bg-orange-500 text-white" : "bg-gray-200 text-gray-700"}`}
-                            onClick={() => setView("authors")}
-                        >
-                            Authors
-                        </button>
-                    </div>
-                    <table className="w-full text-left border-collapse">
-                        <tbody>
-                            {view === "companies"
-                                ? data.companies.map((company, index) => (
-                                    <tr key={index} className="border-b">
-                                        <td className="px-4 py-2">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 24 24"
-                                                fill="currentColor"
-                                                className="w-5 h-5 text-orange-500"
-                                            >
-                                                <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
-                                            </svg>
-                                        </td>
-                                        <td className="px-4 py-2">{company.name}</td>
-                                        <td className="px-4 py-2">{company.website}</td>
-                                        <td className="px-4 py-2">
-                                            <div
-                                                className={`w-8 h-8 rounded-full bg-${company.color}-500`}
-                                                title={`${company.color.charAt(0).toUpperCase() + company.color.slice(1)} Team`}
-                                            ></div>
-                                        </td>
-                                        <td className="px-4 py-2 text-center">
-                                            <button
-                                                className={`w-10 h-6 rounded-full mx-auto ${company.active ? "bg-orange-500" : "bg-gray-300"}`}
-                                                onClick={() => toggleActive("companies", index)}
-                                            >
-                                                <span
-                                                    className={`block w-4 h-4 rounded-full bg-white transform transition-transform ${company.active ? "translate-x-5" : "translate-x-1"}`}
-                                                ></span>
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))
-                                : data.authors.map((author, index) => (
-                                    <tr key={index} className="border-b">
-                                        <td className="px-4 py-2">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 24 24"
-                                                fill="currentColor"
-                                                className="w-5 h-5 text-orange-500"
-                                            >
-                                                <path
-                                                    fillRule="evenodd"
-                                                    d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
-                                                    clipRule="evenodd"
-                                                />
-                                            </svg>
-                                        </td>
-
-                                        <td className="px-4 py-2">{author.name}</td>
-                                        <td className="px-4 py-2">{author.date}</td>
-                                        <td className="px-4 py-2">{author.location}</td>
-                                        <td className="px-4 py-2">
-                                            <img
-                                                src={author.photo}
-                                                alt={author.name}
-                                                className="w-8 h-8 rounded-full"
-                                            />
-                                        </td>
-                                        <td className="px-4 py-2 text-center">
-                                            <button
-                                                className={`w-10 h-6 rounded-full mx-auto ${author.active ? "bg-orange-500" : "bg-gray-300"}`}
-                                                onClick={() => toggleActive("authors", index)}
-                                            >
-                                                <span
-                                                    className={`block w-4 h-4 rounded-full bg-white transform transition-transform ${author.active ? "translate-x-5" : "translate-x-1"}`}
-                                                ></span>
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))}
-                        </tbody>
-                    </table>
+            {/* Table */}
+            <div className="border rounded-t-lg shadow-md bg-white w-full mx-auto overflow-x-auto">
+                <div className="flex justify-center border-none rounded-t-lg">
+                    <button
+                        className={`px-4 py-2 rounded-tl-lg w-full ${view === "companies" ? "bg-orange-500 text-white" : "bg-gray-200 text-gray-700"}`}
+                        onClick={() => setView("companies")}
+                    >
+                        Companies
+                    </button>
+                    <button
+                        className={`px-4 py-2 rounded-tr-lg w-full ${view === "authors" ? "bg-orange-500 text-white" : "bg-gray-200 text-gray-700"}`}
+                        onClick={() => setView("authors")}
+                    >
+                        Authors
+                    </button>
                 </div>
-        
+                <table className="w-full text-left border-collapse">
+                    <tbody>
+                        {view === "companies"
+                            ? data.companies.map((company, index) => (
+                                <tr key={index} className="border-b">
+                                    <td className="px-4 py-2">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 24 24"
+                                            fill="currentColor"
+                                            className="w-5 h-5 text-orange-500"
+                                        >
+                                            <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
+                                        </svg>
+                                    </td>
+                                    <td className="px-4 py-2">{company.name}</td>
+                                    <td className="hidden sm:table-cell px-4 py-2">{company.website}</td>
+                                    <td className="hidden sm:table-cell px-4 py-2">
+                                        <div
+                                            className={`w-8 h-8 rounded-full ${company.color === "blue" ? "bg-blue-500" : company.color === "red" ? "bg-red-500" : company.color === "orange" ? "bg-orange-500" : "bg-purple-500"}`}
+                                            title={`${company.color.charAt(0).toUpperCase() + company.color.slice(1)} Team`}
+                                        />
+                                    </td>
+                                    <td className="px-4 py-2 text-center">
+                                        <button
+                                            className={`w-10 h-6 rounded-full mx-auto ${company.active ? "bg-orange-500" : "bg-gray-300"}`}
+                                            onClick={() => toggleActive("companies", index)}
+                                        >
+                                            <span
+                                                className={`block w-4 h-4 rounded-full bg-white transform transition-transform ${company.active ? "translate-x-5" : "translate-x-1"}`}
+                                            ></span>
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))
+                            : data.authors.map((author, index) => (
+                                <tr key={index} className="border-b">
+                                    <td className="px-4 py-2">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 24 24"
+                                            fill="currentColor"
+                                            className="w-5 h-5 text-orange-500"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                    </td>
+                                    <td className="px-4 py-2 text-sm">{author.name}</td>
+                                    <td className="hidden sm:table-cell px-4 py-2">{author.date}</td>
+                                    <td className="hidden sm:table-cell px-4 py-2">{author.location}</td>
+                                    <td className="hidden sm:table-cell px-4 py-2">
+                                        <img
+                                            src={author.photo}
+                                            alt={author.name}
+                                            className="w-8 h-8 rounded-full"
+                                        />
+                                    </td>
+                                    <td className="px-4 py-2 text-center">
+                                        <button
+                                            className={`w-10 h-6 rounded-full mx-auto ${author.active ? "bg-orange-500" : "bg-gray-300"}`}
+                                            onClick={() => toggleActive("authors", index)}
+                                        >
+                                            <span
+                                                className={`block w-4 h-4 rounded-full bg-white transform transition-transform ${author.active ? "translate-x-5" : "translate-x-1"}`}
+                                            ></span>
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                    </tbody>
+                </table>
+            </div>
         </>
-    )
+    );
 }
--- src/Pages/AdminPanel.jsx
+++ src/Pages/AdminPanel.jsx
@@ -7,20 +7,20 @@ export default function AdminPanel() {
         <div className="flex flex-col h-full justify-center items-center">
             {/* Header with background image */}
             <div
-                className="relative h-[600px] w-full bg-cover bg-center"
+                className="relative h-96 md:h-[600px] w-full bg-cover bg-center"
                 style={{ backgroundImage: `url(${imagePanel})` }}
             >
                 <div className="absolute inset-0 bg-black bg-opacity-50"></div>
                 {/* Main Title */}
                 <div className="absolute inset-0 flex justify-center items-center mb-36">
-                    <h1 className="text-white text-5xl font-bold">Panel</h1>
+                    <h1 className="text-white text-3xl sm:text-4xl md:text-5xl font-bold text-center px-4">Panel</h1>
                 </div>
             </div>
 
             {/* Main Content */}
-            <div className="flex flex-col justify-center items-center relative bg-white p-8 w-11/12 h-auto bottom-56 border border-none rounded-t-lg">
+            <div className="flex flex-col justify-center items-center relative bg-white p-8 pt-14 w-11/12 h-auto bottom-56 border border-none rounded-t-lg">
                 {/* Section Title */}
-                <h2 className="text-center text-3xl font-bold text-orange-500 mb-10 underline decoration-[#f97117] decoration-4 underline-offset-8">
+                <h2 className="text-center text-3xl font-bold text-orange-500 mb-14 underline decoration-[#f97117] decoration-4 underline-offset-8">
                     Entities
                 </h2>
                 {/* Table */}
