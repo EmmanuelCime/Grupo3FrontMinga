bbce0689179c241a58d8b6f36248b40b73e247ff
EmmanuelCime <ruzdemmanuel@outlook.com>
2024-12-06 10:47:41 -0600

    Style modifications

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -49,18 +49,17 @@ function Carousel() {
 
     return (
         <div className="relative max-w-72 md:max-w-2xl lg:max-w-4xl xl:max-w-7xl mx-auto mt-3 mb-2 md:my-8 bg-gradient-to-r from-orange-500 to-orange-400 rounded-lg shadow-lg">
-
             <div className="flex items-center md:p-6">
                 <button onClick={PreviousSlide} className="text-black md:text-2xl md:p-2 pe-1 rounded-full bg-slate-200 shadow-md hover:bg-gray-400 hover:text-white">
                     &larr;
                 </button>
 
-                <div className="flex flex-col md:flex-row items-center h-[320px] md:h-[360px] w-full">
+                <div className="flex flex-col md:flex-row items-center h-[320px] md:h-[340px] w-full">
                     <div className="flex-1">
-                        <img src={slides[currentIndex].img} alt={slides[currentIndex].title} className="h-36 object-contain md:ps-2" />
+                        <img src={slides[currentIndex].img} alt={slides[currentIndex].title} className="h-36 xl:h-60 object-contain md:ps-2" />
                     </div>
                     <div className="flex-1 hidden sm:block">
-                        <img src={slides[currentIndex].poster} alt={slides[currentIndex].title} className="h-56 object-cover mx-auto rounded-lg" />
+                        <img src={slides[currentIndex].poster} alt={slides[currentIndex].title} className="h-64 lg:h-80 xl:h-96 object-cover mx-auto rounded-lg" />
                     </div>
                     <div className="flex-1 text-white md:ml-4 md:me-2">
                         <h2 className="text-center md:text-start text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">{slides[currentIndex].title}</h2>
@@ -72,7 +71,6 @@ function Carousel() {
                     &rarr;
                 </button>
             </div>
-
         </div>
     )
 }
