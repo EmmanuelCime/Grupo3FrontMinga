11b0aa061d883d6a9356403af22f8960785916fc
JonathanAndresM <166529327+JonathanAndresM@users.noreply.github.com>
2024-12-09 16:51:56 -0300

    arreglo simple en sideBar

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -27,8 +27,8 @@ export default function SidebarWithToggle() {
   const navigate = useNavigate()
 
   const lineColor = location.pathname.startsWith("/details") ? "bg-white" : "bg-orange-500";
-  const isUser = user?.role === 0 && token;
-  const isManager = user?.role !== 0 && token;
+  const isUser = user?.role === 0 && token
+  const isManager = user?.role !== 0 && token
 
   const handleLinkClick = () => {
     setIsSidebarOpen(false)
@@ -152,5 +152,5 @@ export default function SidebarWithToggle() {
         </div>
       )}
     </div>
-  );
+  )
 }
